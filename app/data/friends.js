6a888f812189d5a88e12ee40e6b5fcc9f4f9099a
William McLaughlin <williammclaughlin@Williams-MacBook-Pro.local>
2019-02-13 21:17:54 -0600

    added files

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+		name: 'Dak Prescott',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Dak_Prescott.JPG`',
+		scores: [2, 5, 5, 3, 3, 2, 5, 3, 4, 5]
+	},{
+		name: 'Tyler Seguin',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Tyler_Seguin_%2815115357360%29_%28cropped%29.jpg/1200px-Tyler_Seguin_%2815115357360%29_%28cropped%29.jpg',
+		scores: [5, 2, 3, 5, 5, 4, 2, 5, 3, 3]
+	},{
+		name: 'Ian Kinsler',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Ian_Kinsler_on_May_13%2C_2016.jpg/250px-Ian_Kinsler_on_May_13%2C_2016.jpg',
+		scores: [1, 4, 4, 2, 2, 4, 3, 2, 2, 4]
+	},{
+		name: 'Reilly Smith',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Reilly_Smith_2018-02-04_1.jpg/230px-Reilly_Smith_2018-02-04_1.jpg',
+		scores: [4, 1, 2, 3, 4, 3, 1, 5, 5, 4]
+	}];
+
+module.exports = friends;
